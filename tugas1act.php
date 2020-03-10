5efec6c0f46dda1ef57222eab87b532e348e2294
arrandimr <61189026+arrandimr@users.noreply.github.com>
2020-03-10 21:06:54 +0700

    Delete tugas1act.php

--- tugas1act.php
+++
@@ -1,8 +0,0 @@
-<?php
-echo "<center> Nama : ".$_POST['nama']."<br>";
-echo "<center> Email : ".$_POST['email']."<br>";
-date_default_timezone_set('asia/jakarta');
-echo "<center> Anda login pada : ".date("H:i ")."<br>";
-echo date ("l, d-F-Y </center>");
-	
-?>
